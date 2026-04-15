852868c3207b991cfcb97d910dc7138475cf5805
pranes826 <pranes.dev@gmail.com>
2026-04-16 00:54:45 +0530

    Fixed TopNav layout error

--- apps/dashboard-next/app/layout.tsx
+++ apps/dashboard-next/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import Navbar from '@/components/TopNav'
+import TopNav from '@/components/TopNav'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
